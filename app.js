75108d454bf5e8654546ce31c09a7436aff86756
Mark Schaal <markschaal@gmail.com>
2015-07-16 14:46:20 -0400

    Updating homepage feature cards.

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public/images', express.static(path.join(__dirname, 'public/images')));
 app.use('/files', express.static(path.join(__dirname, 'public/files')));
 
 // Define Application Routes
